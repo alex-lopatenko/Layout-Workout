8690d44f91ae29fa34f38f3655b684d2c2d59e54
Alexey <alexey-lopatenko@yandex.ru>
2020-10-15 21:54:29 +0300

    Final deploy

--- app/src/main/java/android/example/layoutworkout/MainActivity.kt
+++ app/src/main/java/android/example/layoutworkout/MainActivity.kt
@@ -1,11 +1,33 @@
 package android.example.layoutworkout
 
+import android.example.layoutworkout.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main)
+
+//        val myButton: Button = findViewById(R.id.button)
+        activityMainBinding.button.setOnClickListener{changeText()}
+    }
+
+    private fun changeText() {
+//        val myTextView: TextView = findViewById(R.id.textView)
+
+//        myTextView.text = "Data Binding"
+
+        activityMainBinding.textView.text = "Data Binding"
     }
 }
